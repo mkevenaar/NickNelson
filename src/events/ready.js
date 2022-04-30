f38c8ab467b03389f2b62a90adf1b848f3c6fbf1
Maurice Kevenaar <maurice@kevenaar.name>
2022-04-30 13:58:31 +0200

    (maint) add ready status

---
+++ src/events/ready.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name: 'ready',
+	once: true,
+	execute(client) {
+		console.log('Logged as:', client.user.tag);
+		client.user.setPresence({ activities: [{ name: 'I like Charlie Spring! In a romantic way not just a friend way!' }], status: 'online' });
+	},
+};
