c2910c6f209cf68b4ecedc17fb7b696bdb58a2c4
Maurice Kevenaar <maurice@kevenaar.name>
2022-05-03 21:27:10 +0200

    needs reword / undo

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -2,8 +2,15 @@ import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
 	id: { type: String },
-	registeredAt: { type: Number, default: Date.now() },
+	guild: { type: String },
+	registeredAt: { type: Number, default: Date.now }, // `Date.now()` returns the current unix timestamp as a number
 	reputation: { type: Number, default: 0 },
+	reason: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "reason"
+		  }
+	],
 });
 
 export const UserModel = mongoose.model('user', UserSchema);
--- src/database/user.service.js
+++ src/database/user.service.js
@@ -4,14 +4,15 @@ export class UserService {
 	/**
 	 * Get or Create a User entry based on its ID
 	 */
-	static async fetchUser(key) {
-		let userEntry = await UserModel.findOne({ id: key });
+	static async fetchUser(key, guildId) {
+		let userEntry = await UserModel.findOne({ id: key, guild: guildId });
 		if (userEntry) {
 			return userEntry;
 		}
 
 		userEntry = new UserModel({
 			id: key,
+			guild: guildId,
 			registeredAt: Date.now(),
 		});
 		await userEntry.save().catch((err) => console.log(err));
