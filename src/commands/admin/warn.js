7a9d312d16f241ad0f5211f021fdfe80d7162083
Lucxjo <Lucxjo@users.noreply.github.com>
2022-06-14 18:13:51 +0000

    Prettified Code!

--- src/commands/admin/warn.js
+++ src/commands/admin/warn.js
@@ -2,36 +2,36 @@ import { SlashCommandBuilder } from '@discordjs/builders';
 import { MessageEmbed, Permissions } from 'discord.js';
 
 export const data = new SlashCommandBuilder()
-	.setName('warn')
-	.setDescription('Warns a user.')
-	.addUserOption((option) =>
-		option.setName('user').setDescription('The user to warn.').setRequired(true)
-	);
+  .setName('warn')
+  .setDescription('Warns a user.')
+  .addUserOption((option) =>
+    option.setName('user').setDescription('The user to warn.').setRequired(true)
+  );
 
 export async function execute(interaction, client) {
-	const userService = client.database.UserService;
+  const userService = client.database.UserService;
 
-	if (!interaction.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
-		await interaction.reply({
-			content: "You don't have permission to do that!",
-			ephemeral: true,
-		});
-		return;
-	}
+  if (!interaction.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
+    await interaction.reply({
+      content: "You don't have permission to do that!",
+      ephemeral: true,
+    });
+    return;
+  }
 
-	let data = await userService.fetchUser(interaction.user.id);
-	if (!data.reputation) {
-		data.reputation = -1;
-	} else {
-		data.reputation--;
-	}
+  let data = await userService.fetchUser(interaction.user.id);
+  if (!data.reputation) {
+    data.reputation = -1;
+  } else {
+    data.reputation--;
+  }
 
-	await data.save();
+  await data.save();
 
-	const user = interaction.options.getUser('user');
-	const embed = new MessageEmbed()
-		.setColor('#0099ff')
-		.setTitle('Warn!')
-		.setDescription(`User warned: ${user.username}`);
-	await interaction.reply({ embeds: [embed], ephemeral: true });
+  const user = interaction.options.getUser('user');
+  const embed = new MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle('Warn!')
+    .setDescription(`User warned: ${user.username}`);
+  await interaction.reply({ embeds: [embed], ephemeral: true });
 }
